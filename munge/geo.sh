6b489b7f07183fa40a1c5c992f37e238fce1037d
thfield <thfield@gmail.com>
2017-09-15 15:10:29 -0700

    pipeline for geography

--- munge/geo.sh
+++ munge/geo.sh
@@ -1,24 +1,61 @@
 #!/bin/bash
-# requires ogr2ogr, node
+# requires:
+#   node (nodejs.org)
+#   npm (npmjs.org)
+#   topojson (https://github.com/topojson/topojson)
+#   ndjson-cli (https://github.com/mbostock/ndjson-cli)
+#   shapefile (https://github.com/mbostock/shapefile)
+# following https://medium.com/@mbostock/command-line-cartography-part-1-897aa8f8ca2c
 
 cd ./data
-rm -rvf geo
-mkdir -p raw-geo
-mkdir -p geo
 
-if [ ! -f raw-geo/cb_2015_us_county_5m.shp ]; then
-  curl -o raw-geo/cb_2015_us_county_5m.zip 'https://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_us_county_5m.zip'
-  unzip raw-geo/cb_2015_us_county_5m.zip -d raw-geo
-fi
+# rm -rvf geo
+# mkdir -p raw-geo
+# mkdir -p geo
+#
+# if [ ! -f raw-geo/cb_2015_us_county_5m.shp ]; then
+#   curl -o raw-geo/cb_2015_us_county_5m.zip 'https://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_us_county_5m.zip'
+#   unzip raw-geo/cb_2015_us_county_5m.zip -d raw-geo
+# fi
+#
+# if [ ! -f raw-geo/cb_2015_us_state_5m.shp ]; then
+#   curl -o raw-geo/cb_2015_us_state_5m.zip 'https://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_us_state_5m.zip'
+#   unzip raw-geo/cb_2015_us_state_5m.zip -d raw-geo
+# fi
+#
+shp2json raw-geo/cb_2015_us_county_5m.shp -o geo/counties.geojson
+shp2json raw-geo/cb_2015_us_state_5m.shp -o geo/states.geojson
 
-if [ ! -f raw-geo/cb_2015_us_state_5m.shp ]; then
-  curl -o raw-geo/cb_2015_us_state_5m.zip 'https://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_us_state_5m.zip'
-  unzip raw-geo/cb_2015_us_state_5m.zip -d raw-geo
-fi
+# turn into NDJSON format
+# filter only 50 states + DC
+# get rid of extra properties
+ndjson-split 'd.features' \
+  < geo/states.geojson \
+  | ndjson-filter 'd.properties.STATEFP < 57 && d.properties.STATEFP > 0' \
+      | ndjson-map 'd.properties = {statefp:d.properties.STATEFP, usps:d.properties.STUSPS, name:d.properties.NAME}, d' \
+          > geo/states.ndjson
 
-ogr2ogr -f GeoJSON -t_srs crs:84 geo/counties.geojson raw-geo/cb_2015_us_county_5m.shp
-ogr2ogr -f GeoJSON -t_srs crs:84 geo/states.geojson raw-geo/cb_2015_us_state_5m.shp
+# reformat from ndjson -> geojson
+ndjson-reduce \
+  < geo/states.ndjson \
+  | ndjson-map '{type: "FeatureCollection", features: d}' \
+  > geo/states.geojson
 
-cd ../munge
+# transform to topojson format
+geo2topo geo/states.geojson > geo/states.topojson
 
-node centroids.js
+toposimplify -S 0.1 -F \
+  < geo/states.topojson \
+  > geo/states-simple.topojson
+
+topoquantize 1e5 \
+  < geo/states-simple.topojson \
+  > geo/states-quantized.topojson
+
+# clean up
+rm geo/states.ndjson geo/states.topojson geo/states-simple.topojson
+mv geo/states-quantized.topojson geo/states.topojson
+
+# cd ../munge
+
+# node centroids.js
